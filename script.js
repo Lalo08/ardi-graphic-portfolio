b2bb6ed97bfd376ad86b03941c89788ecedb7123
Lalo08 <72711554+Lalo08@users.noreply.github.com>
2024-08-14 07:08:02 +0700

    portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+// Simple scroll effect for smooth navigation
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
